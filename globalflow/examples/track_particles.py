8a21750e90cd987a84d272caba861987bf10d0dd
Christoph.Heindl <cheind@profactor.at>
2021-07-22 15:45:54 +0200

    different model needed

--- globalflow/examples/track_particles.py
+++ globalflow/examples/track_particles.py
@@ -7,9 +7,11 @@ from ..mot import GlobalFlowMOT, flowdict_to_trajectories
 
 
 def sample_trajectory(tmax, td):
-    xy_sample = np.random.uniform(0, 10, size=(4, 2))
-    t_start = np.random.uniform(0, tmax / 4.0)
-    t_stop = np.random.uniform(3.0 * tmax / 4, tmax)
+    xy_sample = np.random.uniform(0, 100, size=(4, 2))
+    # t_start = np.random.uniform(0, tmax / 4.0)
+    # t_stop = np.random.uniform(3.0 * tmax / 4, tmax)
+    t_start = 0
+    t_stop = tmax
     t_sample = np.linspace(t_start, t_stop, 4)
     fx = interp1d(t_sample, xy_sample[:, 0], kind="cubic")
     fy = interp1d(t_sample, xy_sample[:, 1], kind="cubic")
@@ -44,16 +46,16 @@ def main():
         if np.isnan(x.obs[0]) or np.isnan(y.obs[1]):
             return np.log(1e-8)
         else:
-            logx = scipy.stats.norm.logpdf(y.obs[0], loc=x.obs[0], scale=3.0)
-            logy = scipy.stats.norm.logpdf(y.obs[1], loc=x.obs[1], scale=3.0)
-            print(x.obs, y.obs, logx, logy)
+            logx = scipy.stats.norm.logpdf(y.obs[0], loc=x.obs[0], scale=2.0)
+            logy = scipy.stats.norm.logpdf(y.obs[1], loc=x.obs[1], scale=2.0)
+            print(x.obs, y.obs, logx + logy, x, y)
             return logx + logy
 
     def logp_enter(xi):
-        return 0.0 if xi.time_index == 0 else -500
+        return 0.0 if xi.time_index == 0 else -300
 
     def logp_exit(xi):
-        return 0.0 if xi.time_index == len(dets) - 1 else -500
+        return 0.0 if xi.time_index == len(dets) - 1 else -300
 
     flow = GlobalFlowMOT(
         dets,
@@ -63,7 +65,8 @@ def main():
         logp_tp_fn=lambda x: np.log(0.99),
         logprob_cutoff=-1e5,
     )
-    flowdict, ll = flow.solve((1, 8))
+    print(dets)
+    flowdict, ll = flow.solve()
     print(flowdict_to_trajectories(flow, flowdict))
     from ..draw import draw_graph, draw_flowdict
 
